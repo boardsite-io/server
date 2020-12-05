19da795ef7566f20b3cc24b7c4fb7276fc73df37
Patrick Willner <patrick.willner@tum.de>
2020-12-05 13:30:02 +0100

    Set port and db host via cli args

--- cmd/boardsite/main.go
+++ cmd/boardsite/main.go
@@ -8,21 +8,34 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 
 	"github.com/heat1q/boardsite/app"
 )
 
 func main() {
-	port := flag.Int("port", 8000, "listening on port")
+	portStr := flag.String("port", os.Getenv("B_PORT"), "listening on port")
+	redisHost := flag.String("redis-host", os.Getenv("B_REDIS_HOST"), "redis hostname")
+	redisPort := flag.String("redis-port", os.Getenv("B_REDIS_PORT"), "redis port")
 	flag.Parse()
+
+	// input args may overwrite os env
+	os.Setenv("B_PORT", *portStr)
+	os.Setenv("B_REDIS_HOST", *redisHost)
+	os.Setenv("B_REDIS_PORT", *redisPort)
+
+	port, err := strconv.Atoi(*portStr)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	gracefulStop := make(chan os.Signal)
 	signal.Notify(gracefulStop, syscall.SIGTERM)
 	signal.Notify(gracefulStop, syscall.SIGINT)
 
 	ctx := context.Background()
-	run, shutdown := app.Serve(ctx, *port)
+	run, shutdown := app.Serve(ctx, port)
 	defer shutdown()
 
 	go func() {
