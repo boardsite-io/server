a413c957804a2b2190da97404c4a7db8da8c4fab
Patrick Willner <50421879+heat1q@users.noreply.github.com>
2022-01-18 20:39:51 +0100

    Adjust rate limiting

--- api/routes.go
+++ api/routes.go
@@ -11,12 +11,12 @@ func (s *Server) setRoutes() {
 	boardGroup := s.echo.Group("/b", middleware.Monitoring(), middleware.RequestLogger())
 	boardGroup.POST( /*  */ "/create", s.session.PostCreateSession, middleware.RateLimiting(1, middleware.WithIP()))
 
-	usersGroup := boardGroup.Group("/:id/users", middleware.RateLimiting(rpmSession, middleware.WithUserIP()))
+	usersGroup := boardGroup.Group("/:id/users")
 	usersGroup.POST( /*  */ "", s.session.PostUsers)
 	usersGroup.GET( /*   */ "", s.session.GetUsers, middleware.Session(s.dispatcher))
 	usersGroup.GET( /*   */ "/:userId/socket", s.session.GetSocket, middleware.Session(s.dispatcher))
 
-	pagesGroup := boardGroup.Group("/:id/pages", middleware.Session(s.dispatcher), middleware.RateLimiting(rpmSession, middleware.WithUserID()))
+	pagesGroup := boardGroup.Group("/:id/pages", middleware.Session(s.dispatcher))
 	pagesGroup.GET( /*   */ "", s.session.GetPages)
 	pagesGroup.POST( /*  */ "", s.session.PostPages)
 	pagesGroup.PUT( /*   */ "", s.session.PutPages)
@@ -25,6 +25,6 @@ func (s *Server) setRoutes() {
 	pagesGroup.DELETE( /**/ "/:pageId", s.session.DeletePageUpdate)
 
 	attachGroup := boardGroup.Group("/:id/attachments", middleware.Session(s.dispatcher))
-	attachGroup.POST( /**/ "", s.session.PostAttachment, middleware.RateLimiting(1, middleware.WithUserID()))
+	attachGroup.POST( /**/ "", s.session.PostAttachment, middleware.RateLimiting(1, middleware.WithUserIP()))
 	attachGroup.GET( /* */ "/:attachId", s.session.GetAttachment)
 }
