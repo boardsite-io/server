733dd3d00a64304d1fd458975e9f5c56dcf1f554
Patrick Willner <50421879+heat1q@users.noreply.github.com>
2022-03-20 22:02:04 +0100

    Simplify errors

--- api/errors/errors.go
+++ api/errors/errors.go
@@ -7,31 +7,27 @@ import (
 )
 
 var (
-	ErrBadRequest   = New(http.StatusBadRequest)
-	ErrUnauthorized = New(http.StatusUnauthorized)
-	ErrForbidden    = New(http.StatusForbidden)
-	ErrNotFound     = New(http.StatusNotFound)
-
+	ErrBadRequest          = New(http.StatusBadRequest)
+	ErrUnauthorized        = New(http.StatusUnauthorized)
+	ErrForbidden           = New(http.StatusForbidden)
+	ErrNotFound            = New(http.StatusNotFound)
 	ErrInternalServerError = New(http.StatusInternalServerError)
 	ErrBadGateway          = New(http.StatusBadGateway)
-	ErrGatewayTimeout      = New(http.StatusGatewayTimeout)
 )
 
 type HTTPError struct {
 	Status   int    `json:"-"`
 	Message  string `json:"message"`
+	Code     Code   `json:"code,omitempty"`
 	internal error
-	Code     Code `json:"code,omitempty"`
 }
 
 var _ error = (*HTTPError)(nil)
 
-// New creates a new HTTPError error with the given status and optional message.
-// If no message is provided, the default http.StatusText is set as message.
 func New(status int, message ...string) *HTTPError {
 	msg := http.StatusText(status)
 	if len(message) > 0 {
-		msg = strings.Join(message, ", ")
+		msg = message[0]
 	}
 	return &HTTPError{
 		Status:  status,
@@ -52,38 +48,12 @@ func From(code Code) *HTTPError {
 
 type ErrorOption func(e *HTTPError)
 
-// WithMessage overwrites the default http.StatusText in the message field.
-// This functional argument is passed to the HTTPError.Wrap method.
-func WithMessage(msg string) ErrorOption {
-	return func(e *HTTPError) {
-		e.Message = msg
-	}
-}
-
-// WithMessagef formats and overwrites the default http.StatusText in the message field.
-// This functional argument is passed to the HTTPError.Wrap method.
-func WithMessagef(format string, a ...interface{}) ErrorOption {
-	return WithMessage(fmt.Sprintf(format, a...))
-}
-
-// WithError sets the reason of the error, used for internal logging.
-// This functional argument is passed to the HTTPError.Wrap method.
 func WithError(err error) ErrorOption {
 	return func(e *HTTPError) {
 		e.internal = err
 	}
 }
 
-// WithCode sets the error code.
-// This functional argument is passed to the HTTPError.Wrap method.
-func WithCode(code Code) ErrorOption {
-	return func(e *HTTPError) {
-		e.Code = code
-	}
-}
-
-// WithErrorf formats an internal error.
-// This functional argument is passed to the HTTPError.Wrap method.
 func WithErrorf(format string, a ...interface{}) ErrorOption {
 	return WithError(fmt.Errorf(format, a...))
 }
@@ -108,7 +78,6 @@ func (e HTTPError) Is(err error) bool {
 	return e.Status == target.Status && e.Code == target.Code
 }
 
-// Wrap wraps any optional argument in the HTTPError
 func (e HTTPError) Wrap(options ...ErrorOption) error {
 	for _, o := range options {
 		o(&e)
@@ -116,7 +85,6 @@ func (e HTTPError) Wrap(options ...ErrorOption) error {
 	return &e
 }
 
-// Unwrap unwraps the internal error of HTTPError.
 func (e *HTTPError) Unwrap() error {
 	return e.internal
 }
